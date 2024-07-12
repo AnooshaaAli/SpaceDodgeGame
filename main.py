2b75602d23e1110508de6a9b23da38281f0e291e
AnooshaaAli <anooshasixteen@gmail.com>
2024-07-12 18:26:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ import time
 import random
 
 WIDTH, HEIGHT = 1000, 800
-WIN = pygame.display.set_mode(WIDTH, HEIGHT)
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))  
+pygame.display.set_caption("Space Dodge")
